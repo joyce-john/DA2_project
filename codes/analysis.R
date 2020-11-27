52d03bb4d8d9d63d099e893ea1498236b7ad193a
John Joyce <Joyce_John@student.ceu.edu>
2020-11-27 23:45:05 +0100

    incremental misc progress

--- codes/analysis.R
+++ codes/analysis.R
@@ -1,2 +1,34 @@
-### draft analysis
+########################
+## Assignment for DA2 ##
+##  and for Coding    ##
+##                    ##
+##   NO. 3            ##
+## Analyze   data     ##
+########################
+
+
+# Clear memory and call packages
+rm(list=ls())
+library(tidyverse)
+library(scales)
+
+# Set URL for clean data
+my_url <- "https://raw.githubusercontent.com/joyce-john/DA2_project/main/data/clean/covid_pop_09_11_2020_clean.csv"
+
+# Read the clean data into a dataframe
+df <- read.csv(my_url)
+
+# Delete the URL variable
+rm(my_url)
+
+summary(df)
+
+df %>%
+  keep(is.numeric) %>% 
+  gather() %>% 
+  ggplot(aes(value)) +
+  facet_wrap(~key, scales = "free") +
+  geom_histogram()
+
+
 
