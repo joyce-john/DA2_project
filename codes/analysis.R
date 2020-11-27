f141b4ba887e11a9bf6b20462f83c0db02165622
John Joyce <Joyce_John@student.ceu.edu>
2020-11-28 00:45:52 +0100

    graphed models with different transformations

--- codes/analysis.R
+++ codes/analysis.R
@@ -30,5 +30,39 @@ df %>%
   facet_wrap(~key, scales = "free") +
   geom_histogram()
 
+# level-level
+ggplot( df , aes(x = death, y = confirmed)) +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "confirmed (level) )",y = "deaths (level)")
+
+# log x - level y
+ggplot( df , aes(x = death, y = confirmed)) +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "confirmed (log) )",y = "deaths (level)") + 
+  scale_x_continuous( trans = log_trans(),  breaks = c(1,2,5,10,20,50,100,200,500,1000,10000) )
+
+# level x - log y
+ggplot( df , aes(x = death, y = confirmed)) +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "confirmed (level) )",y = "deaths (log)") + 
+  scale_y_continuous( trans = log_trans(),  breaks = c(1,2,5,10,20,50,100,200,500,1000,10000) )
+
+# log x - log y
+ggplot( df , aes(x = death, y = confirmed)) +
+  geom_point() +
+  geom_smooth(method="loess")+
+  labs(x = "confirmed (log) )",y = "deaths (log)") + 
+  scale_x_continuous( trans = log_trans(),  breaks = c(1,2,5,10,20,50,100,200,500,1000,10000) )+
+  scale_y_continuous( trans = log_trans() )
+
+##### log-log model looks promising #####
+
+# Mutate dataframe to include logs
+df <- df %>% 
+      mutate(log_confirmed = log(confirmed),
+             log_death = log(death))
 
 
